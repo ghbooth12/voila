7c9e282a52fb78753fc9a2de4ab8dc323e02c4bc
Gahee Heo <ghbooth12@gmail.com>
2017-02-12 12:38:23 -0600

    Style navbar and page scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,13 @@
+// Parallax background effect
 $(function() {
 	//Cache the window object
 	var $window = $(window);
 
-	//Parallax backgroun effect
 	$('section[data-type="background"]').each(function() {
 		var $bgobj = $(this); //assigning the object
-
 		$window.scroll(function() {
 			//scroll the background at var speed
 			//the yPos is a negative value because we're scrolling it UP!
-
 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
 
 			//Put together our final background position
@@ -20,3 +18,26 @@ $(function() {
  		});
 	});
 });
+
+
+// Nav collapse when scroll down
+$(window).scroll(function(){
+	if($('.navbar').offset().top > 50) {
+		$('.navbar-fixed-top').addClass('top-nav-collapse');
+	} else {
+		$('.navbar-fixed-top').removeClass('top-nav-collapse');
+	}
+});
+
+
+// Page Scroll Effect
+$(function(){
+	$('a.page-scroll').bind('click', function(event){
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1250, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
+
